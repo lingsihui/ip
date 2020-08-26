0944f304cfb0dbd6f9184f08639cec20555750a7
lingsihui <e0426314@u.nus.edu>
2020-08-26 23:03:34 +0800

    Added greet echo exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,6 +11,17 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        String upperLine = line.toUpperCase();
+        if (upperLine.equals("BYE")){
+            System.out.println("Bye. Hope to see you again soon!\n");
+        }
+        else{
+            System.out.print(line);
+        }
+
     }
 }
