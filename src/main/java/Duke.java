1080abf9e0ec12be59535b31793d263a67597e6e
lingsihui <e0426314@u.nus.edu>
2020-08-27 21:42:41 +0800

    Add Coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -15,40 +14,36 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         //Reading inputs
-        int exit = 0;
+        int isExited = 0;
         int numOfList = 0;
-        Task list[] = new Task [100] ;
+        Task[] List = new Task [100] ;
 
         do{
             String line;
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
             String upperLine = line.toUpperCase();
-            //user enter bye
             if (upperLine.equals("BYE")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
-                exit = 1;
-            }
-            else {
+                isExited = 1;
+            } else {
                 if (upperLine.equals("LIST")) {
                     for (int i = 0; i < numOfList;i++) {
-                        System.out.println(i+1 + "." + list[i].getStatusIcon() + " " + list[i].description);
+                        System.out.println(i+1 + "." + List[i].getStatusIcon() + " " + List[i].description);
                     }
-                }
-                else if(upperLine.contains("DONE")){
+                } else if(upperLine.contains("DONE")){
                     String[] words = line.split(" ");
                     int taskNum = Integer.parseInt(words[1]);
-                    list[taskNum-1].markAsDone();
+                    List[taskNum-1].markAsDone();
                     System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println(list[taskNum-1].getStatusIcon() + " " + list[taskNum-1].description);
-                }
-                else {
+                    System.out.println(List[taskNum-1].getStatusIcon() + " " + List[taskNum-1].description);
+                } else {
                     System.out.println("added: " + line);
-                    list[numOfList] = new Task(line);
+                    List[numOfList] = new Task(line);
                     numOfList++;
                 }
 
             }
-        } while ( exit == 0);
+        } while (isExited == 0);
     }
 }
